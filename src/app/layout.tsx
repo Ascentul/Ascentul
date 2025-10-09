f2b5fd233384df496f056f30ee8a9ff83107e74d
asvirts <asvirts@outlook.com>
2025-10-09 10:06:41 -0500

    favicon and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,8 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" href="/logo.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         </head>
